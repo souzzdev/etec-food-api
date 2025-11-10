e1ad7407f58d8eb612002344ac5ed40b488350c1
souzzdev <ninjasdttn@gmail.com>
2025-11-10 00:04:03 -0300

    Configuração do application.properties

--- src/main/java/br/com/etechas/etecfood/controller/EntregaController.java
+++ src/main/java/br/com/etechas/etecfood/controller/EntregaController.java
@@ -1,4 +1,64 @@
 package br.com.etechas.etecfood.controller;
 
+import br.com.etechas.etecfood.entity.Entrega;
+import br.com.etechas.etecfood.repository.EntregaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/entrega")
 public class EntregaController {
+    @Autowired
+    EntregaRepository entregaRepository;
+
+    @GetMapping
+    public List<Entrega> findAll() {
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Entrega> findById(@PathVariable Integer id) {
+        var entrega = entregaRepository.findById(id);
+
+        if(entrega.isPresent()) return ResponseEntity.ok(entrega.get());
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Entrega> cadastrar(@RequestBody Entrega entrega) {
+        Entrega novaEntrega = entregaRepository.save(entrega);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaEntrega);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+        var entrega = entregaRepository.findById(id);
+        if(entrega.isPresent()) {
+            entregaRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Entrega> atualizar(@PathVariable Integer id, @RequestBody Entrega entregaAtualizada) {
+        Optional<Entrega> entregaOptional = entregaRepository.findById(id);
+
+        if(entregaOptional.isPresent()) {
+            Entrega entrega = entregaOptional.get();
+
+            entrega.setCep(entregaAtualizada.getCep());
+            entrega.setComplemento(entregaAtualizada.getComplemento());
+            entrega.setEndereco(entregaAtualizada.getEndereco());
+
+            entregaRepository.save(entrega);
+            return ResponseEntity.ok(entrega);
+        }
+        else return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/br/com/etechas/etecfood/repository/EntregaRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/EntregaRepository.java
@@ -1,4 +1,7 @@
 package br.com.etechas.etecfood.repository;
 
-public interface EntregaRepository {
+import br.com.etechas.etecfood.entity.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Integer> {
 }
